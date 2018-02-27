2ba2ea7ebf59241fbf615e8b6533a62c5be50889
Thiago da Rosa de Bustamante <trbustamante@gmail.com>
2018-02-27 15:17:17 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const express = require('express');
+const humanInterval = require('human-interval');
+const fs = require('fs');
+
+const props = JSON.parse(fs.readFileSync(`${__dirname}/properties.json`).toString());
+
+const app = express();
+
+let requests = 0;
+setInterval(() => {
+    requests = 0;
+}, humanInterval(props.timeWindow));
+
+const makeServerSlow = (req, res, next) => {
+    if (requests++ > props.maxRequests) {
+        setTimeout(next, humanInterval(props.slowTime));
+    } else {
+        next();
+    }
+};
+app.use(makeServerSlow);
+
+app.get('/users/:userId', (req, res, next) => {
+    res.json({
+        userId: req.params.userId,
+        name: `Mr. User number ${req.params.userId}`
+    });
+});
+const port = 3000;
+app.listen(port, () => {
+    console.info(`Server listening port ${port}`)
+});
